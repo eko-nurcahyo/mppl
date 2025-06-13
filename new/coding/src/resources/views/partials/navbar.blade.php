98209ff74eecbffb54e543e027563ad7d93c971a
FiqriFathurrohman <fiqrifathurrohman36@gmail.com>
2025-06-13 21:07:59 +0700

    first commit

--- new/coding/src/resources/views/partials/navbar.blade.php
+++ new/coding/src/resources/views/partials/navbar.blade.php
@@ -9,9 +9,9 @@
     </div>
 
     <nav class="navbar navbar-expand-lg navbar-dark py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
-        <a href="{{ url('/') }}" class="navbar-brand ms-4 ms-lg-0">
-            <h1 class="fw-bold text-primary m-0">Chari<span class="text-white">Team</span></h1>
-        </a>
+       <a href="{{ url('/') }}" class="navbar-brand">
+    <img src="{{ asset('assets/charitee/img/eko.png') }}" alt="ChariTeam Logo" height="80">
+</a>
         <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
             <span class="navbar-toggler-icon"></span>
         </button>
